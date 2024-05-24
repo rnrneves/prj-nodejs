d59815547218d4e7bf8099b0d256d3301917f59b
Raimundo Neves <rnrocha@outlook.pt>
2024-05-24 17:37:57 -0300

    Alterada a mensagem de boas vindas

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Bem Vindo Raimnudo Neves !-Agora sim-SUCESSO!!");
+    response.end("Seja Muito Bem Vindo Sr. Raimnudo Neves !-Agora sim-SUCESSO!!");
 });
 
 const port = process.env.PORT || 1337;
